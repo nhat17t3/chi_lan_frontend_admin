66473334753360bd9da441efe1cca1d5b5d427ac
nhat17t3 <58282021+nhat17t3@users.noreply.github.com>
2023-04-25 00:15:24 +0700

    Update urlConfig.js

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,6 +1,6 @@
 // const baseUrl = process.env.API || "https://"
-const baseUrl = "http://be1.tranlan.tk";
-// const baseUrl = "https://mobilelemanh.herokuapp.com";
+//const baseUrl = "http://be1.tranlan.tk";
+const baseUrl = "https://hoanglongnhat-nodejs-ecommerce.onrender.com";
 
 export const api = `${baseUrl}/api`;
 
